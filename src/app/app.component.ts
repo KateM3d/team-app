afc57fef699bdd0a18067c2f0fbaab83ba2b3947
KateM3d <ekaterina8medvedeva@gmail.com>
2022-09-05 15:09:12 -0400

    add list of members

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,22 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'team-app';
+
+  newMemberName: string = '';
+  members: string[] = [];
+
+  onInput(member: string) {
+    this.newMemberName = member;
+    console.log(this.newMemberName);
+  }
+
+  addNewMember() {
+    this.members.push(this.newMemberName);
+    console.log(this.members);
+    this.newMemberName = '';
+  }
 }
